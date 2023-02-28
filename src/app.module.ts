6789cc10fd2eeb62cb04b27b144bc2ff7d946abb
Neel-Wappnet <neel.wappnet@gmail.com>
2023-02-28 17:27:46 +0530

    pipes, exception filter, guard etc

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { loggerMiddleware } from './cats/middleware/logger.middleware';
 import { CatsController } from './cats/cats.controller';
+import { HttpExceptionFilter } from './cats/filter/http-exception.filter';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './cats/guard/roles.guard';
 
 @Module({
   imports: [CatsModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -7,23 +7,43 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  UseFilters,
+  UseGuards,
+  SetMetadata,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './interface/cat.interface';
 import { ForbiddenException } from './exception/forbidden.exception';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
+import { RolesGuard } from './guard/roles.guard';
+import { Roles } from './decorators/roles.decorator';
+import { LoggingInterceptor } from './interceptor/logging.interceptor';
 
 @Controller('cats')
+// @UseGuards(RolesGuard)
+// @SetMetadata('roles', ['admin'])
+@UseInterceptors(LoggingInterceptor)
+@Roles('admin')
+@UseFilters(new HttpExceptionFilter())
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
+    throw new ForbiddenException();
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
+  async findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
     return this.catsService.findOne(id);
   }
 
